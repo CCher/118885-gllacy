3ccecc967fda292f6e4bb0640f226fe21702be49
Constantine Cher <homevent@gmail.com>
2019-01-21 11:31:29 +0600

    Доработка проекта по дополнительным критериям
    Доработка проекта по дополнительным критериям

--- js/script.js
+++ js/script.js
@@ -11,43 +11,42 @@ var storageName = localStorage.getItem("name");
 var storageEmail = localStorage.getItem("email");
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("show-modal");
-  mask.classList.add("visible");
+    evt.preventDefault();
+    popup.classList.add("show-modal");
+    mask.classList.add("visible");
 
-  if (storageName && storageEmail) {
-    name.value = storageName;
-    email.value = storageEmail;
-  }
+    if (storageName && storageEmail) {
+        name.value = storageName;
+        email.value = storageEmail;
+    }
 });
 
 close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("show-modal");
-  mask.classList.remove("visible");
-  popup.classList.remove("error-modal");
+    evt.preventDefault();
+    popup.classList.remove("show-modal");
+    mask.classList.remove("visible");
+    popup.classList.remove("error-modal");
 });
 
 form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if (!name.value || !email.value || !message.value) {
     evt.preventDefault();
-    popup.classList.remove("error-modal");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("error-modal");
-  }
-  else {
-    localStorage.setItem("name", name.value);
-    localStorage.setItem("email", email.value);
-  }
+    if (!name.value || !email.value || !message.value) {
+        evt.preventDefault();
+        popup.classList.remove("error-modal");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("error-modal");
+    } else {
+        localStorage.setItem("name", name.value);
+        localStorage.setItem("email", email.value);
+    }
 });
 
 window.addEventListener("keydown", function (evt) {
-if (evt.keyCode === 27) {
-  if (popup.classList.contains("show-modal")) {
-    popup.classList.remove("show-modal");
-    popup.classList.remove("error-modal");
-    mask.classList.remove("visible");
-  }
-}
+    if (evt.keyCode === 27) {
+        if (popup.classList.contains("show-modal")) {
+            popup.classList.remove("show-modal");
+            popup.classList.remove("error-modal");
+            mask.classList.remove("visible");
+        }
+    }
 });
